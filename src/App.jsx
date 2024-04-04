7eead32c2098356ecb33e4782b54c7638b7061d2
user <user@users-MacBook-Air.local>
2024-04-04 21:32:05 +0700

    build: add animation library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // import reactLogo from '/assets/react.svg'
 // import viteLogo from '/assets/vite.svg'
+import { useEffect } from 'react';
 import './App.css'
 import Index from './pages/ToDo'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
 
   return (
     <>
