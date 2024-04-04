ffd2156103e8779ebcfb96d213cf5488976adf77
user <user@users-MacBook-Air.local>
2024-04-04 17:37:34 +0700

    build: first commit vite - react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import reactLogo from '/assets/react.svg'
+// import viteLogo from '/assets/vite.svg'
+import './App.css'
+import Index from './pages/ToDo'
+
+function App() {
+
+  return (
+    <>
+      <Index />
+    </ >
+  )
+}
+
+export default App
